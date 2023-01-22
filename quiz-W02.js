891b819339b29d7622d828dbdb45defb3477deb5
Idan Fischer <Idanfischer5@gmail.com>
2023-01-22 15:42:00 -0500

    all quiz questions that i used VScode for

---
+++ quiz-W02.js
@@ -0,0 +1,65 @@
+// You are given the following variables:
+// const music = "Duran Duran"
+// const oldWay = "Mom!!" + "Where is my Sony Walkman that has " + music ".";
+// Declare a new variable called "newWay" that converts the above "oldWay" using a template literal. (The output for newWay and oldWay should be the same)
+
+// const music = "Duran Duran"
+// const oldWay = "Mom!!" + "Where is my Sony Walkman that has " + music + ".";
+
+
+// const newWay = `Mom!!Where is my Sony Walkman that has ${music}.`
+
+// console.log(oldWay)
+// console.log(newWay)
+
+
+// Given the following if/else statement, write a ternary.
+
+// const color = "red";
+// color === "red" ? console.log(`Happy Lunar New Year!!`) : console.log(`Colors are cool!`)
+// if (color === "red") { console.log ("Happy Lunar New Year!!"); } else { console.log("Colors are cool!"); }
+
+
+
+//  ["dates","apple","blueberry"] 
+// const yummies = ["apple", "blueberry", "cantaloupe", "dates"]
+// function finishArray(arr) {
+//     arr.pop()
+//     arr.pop()
+//     arr.unshift("dates")
+//     console.log(arr)
+// }
+
+// const result = finishArray(yummies)
+// console.log(result)
+
+// Write a function that runs through each item in an array of vegetables and returns each one. (hint: loops) Console.log your results. 
+
+// let vegetableArray = ["zucchini", "carrot", "lettuce", "potato"]
+
+// const eachVegetable = (arr2) => {
+//     for (i = 0; i < arr2.length; i++) {
+//         console.log(arr2[i])
+//     }
+// }
+
+// eachVegetable(vegetableArray)
+
+// Return the value "daisy" from the array of flowers.
+
+// const flowers =
+//     [
+
+//         {
+//             flower1: "gardenia",
+//             flower2: "daisy",
+//             flower3: "rose"
+//         },
+//         {
+//             flower4: "green",
+//             flower5: "red",
+//             flower6: "blue"
+//         }
+//     ]
+
+// console.log(flowers[0].flower2)
